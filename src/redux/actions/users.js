07c516aa13773a64bcd193c2d04f42f3d28b2077
ryantarisa <ryanantarisara@gmail.com>
2022-09-05 21:49:22 +0800

    adding-register-page
    menambah halaman register beserta reducer dan actionnya

---
+++ src/redux/actions/users.js
@@ -0,0 +1,42 @@
+import Axios from "axios";
+import { API_URL } from "../../constants/API";
+import * as UserApi from "../../API/UserRequest";
+
+// export const registerUser = (data) => {
+//   try {
+//     return async (dispatch) => {
+//       dispatch({
+//         type: "LOADING",
+//         payload: true,
+//       });
+
+//       const response = await Axios.post(`${API_URL}/users/register`, data);
+//     };
+//   } catch (error) {
+//     throw new Error(err.response.data.message);
+//   }
+// };
+
+// export const verifyUser = (data) => {
+//   try {
+//     return async (dispatch) => {
+//       dispatch({
+//         type: "LOADING",
+//         payload: true,
+//       });
+//       const response = await Axios.patch(`${API_URL}/users/verified/:id`, data);
+//     };
+//   } catch (error) {
+//     throw new Error(err.response.data.message);
+//   }
+// };
+
+export const verifyUser = (uuid) => async (dispatch) => {
+  dispatch({ type: "VERIFICATION_START" });
+  try {
+    const { data } = await UserApi.verifyUser(uuid);
+    dispatch({ type: "VERIFICATION_SUCCESS", data: data });
+  } catch (error) {
+    dispatch({ type: "VERIFICATION_FAIL" });
+  }
+};
